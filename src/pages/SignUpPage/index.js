c4d497d987e9c7fa99e22bfe578bb5645035ecb8
colo1211 <colo1211@naver.com>
2021-11-16 22:08:26 +0900

    feat: 회원가입 페이지 작성/데이터 받기 미구현(#6)

--- src/pages/SignUpPage/index.js
+++ src/pages/SignUpPage/index.js
@@ -2,10 +2,24 @@ import React, { useEffect, useState, useRef, useCallback } from 'react'
 import * as S from './style'
 import { useParams } from 'react-router'
 import { CgProfile } from 'react-icons/cg'
+import { DropDown } from '../../components'
+
+import {LandingDropdownOptions} from '../../constants'; 
 
 const SignUp = () => {
+
+
+  useEffect(() => {
+    localStorage.setItem('accessToken', accessToken)
+    return () => {
+      localStorage.setItem('signupCheck', signupCheck)
+    }
+  }, [])
+
   const imgInput = useRef()
   const { accessToken, signupCheck } = useParams()
+
+  // attachment : img URL (for Server)
   const [attachMent, setAttachment] = useState(null)
   const [fileInfo, setFileInfo] = useState(null)
   const [body, setBody] = useState({
@@ -20,32 +34,33 @@ const SignUp = () => {
     imgInput.current.click()
   }, [])
 
-  const onProfileURL = e => {
+  const onProfileURL = useCallback((e)=>{
     const reader = new FileReader()
-    // FormData에 전달해줄 File의 Infomation
     setFileInfo(e.target.files[0])
-
-    reader.readAsDataURL(e.target.files[0]) // e.target.files는 파일을 여러개 선택을 대비하기 위한 API 사용 방법이므로 나는 하나만 할거니까 [0] 으로 선택
-    reader.onloadend = finished => {
-      // reader는 생명주기함수처럼 다룬다. 파일 로드가 끝나면 Attachment state에 img 의 주소를 담으라는 뜻
-      setAttachment(finished.target.result) // 이건 attachment에 URL을 담으라는 뜻, 만약 Img 미리보기를 취소하려면 attachment를 비워주면 된다.
+    reader.readAsDataURL(e.target.files[0]) 
+    reader.onloadend = (finished) => {
+      setAttachment(finished.target.result) 
     }
+  }, []); 
+
+  const onDeleteURL = useCallback(() => {
+    setAttachment(''); 
+  }); 
+
+  const onBodyChange = (e)=>{
+    setBody({
+      ...body,
+      [e.target.name] : e.target.value
+    }) 
+  };
+
+  const onSubmitHandler = (e) =>{
+    e.preventDefault(); 
+    console.log('제출');
   }
-
-  useEffect(() => {
-    localStorage.setItem('accessToken', accessToken)
-    return () => {
-      localStorage.setItem('signupCheck', signupCheck)
-    }
-  }, [])
-
-  useEffect(()=> {
-    console.log(attachMent); 
-  },[attachMent]);
 
   return (
     <>
-      {/* <S.WhiteSpace> </S.WhiteSpace> */}
       <S.SignUpPageWrapper>
         <S.SignUpPageLeft>
           <S.MainTitle color="white" fontSize="50px">
@@ -67,7 +82,10 @@ const SignUp = () => {
           <S.SignUpPageInnerForm>
             <S.ProfileWrapper>
               {attachMent ? (
-                <img src={attachMent} width='30%'/>
+                <>
+                  <img src={attachMent} width="100px"  style={{display : 'block', borderRadius:'5000px'}}/>
+                  <S.CheckBox onClick={onDeleteURL}>X</S.CheckBox>
+                </>
               ) : (
                 <CgProfile size="100" onClick={onProfileUpload} />
               )}
@@ -79,11 +97,36 @@ const SignUp = () => {
                 ref={imgInput}
               />
             </S.ProfileWrapper>
-            <S.MainTitle marginTop="40px" fontSize="20px">
+            
+            {/* 닉네임 */}
+            <S.MainTitle marginTop="7.5%" marginBottom="7.5%" fontSize="20px">
               닉네임
             </S.MainTitle>
-            <S.NickNameBox onChange={{}} />
-            <S.DoubleCheckBox onClick={{}}>중복 체크</S.DoubleCheckBox>
+            <S.NickNameBox name='nickName' onChange={setBody} />
+            <S.CheckBox onClick={()=>{alert('중복체크 중!')}}>중복 체크</S.CheckBox>
+            
+            {/* 신분 */}
+            <S.MainTitle marginTop="7.5%" marginBottom="7.5%" fontSize="20px">
+              신분
+            </S.MainTitle>
+            <S.JobWrapper>
+              <S.JobCheckBtn>학생</S.JobCheckBtn>
+              <S.JobCheckBtn>직장인</S.JobCheckBtn>
+            </S.JobWrapper>
+
+            {/* 기술 스택 */}
+            <S.MainTitle marginTop="7.5%" marginBottom="7.5%" fontSize="20px">
+              기술 스택
+            </S.MainTitle>
+            <DropDown style={{width : '100%'}} options={LandingDropdownOptions}></DropDown>
+
+
+          {/* 자기소개 */}
+          <S.MainTitle marginTop="7.5%" marginBottom="7.5%" fontSize="20px"> 자기소개</S.MainTitle>
+            <S.Introduce />
+
+          <S.SubmitBtn onClick={onSubmitHandler}>회원 가입</S.SubmitBtn>
+            
           </S.SignUpPageInnerForm>
         </S.SignUpPageRight>
       </S.SignUpPageWrapper>
--- src/pages/SignUpPage/style.js
+++ src/pages/SignUpPage/style.js
@@ -1,7 +1,6 @@
 import styled from "styled-components";
 import theme from '../../common/theme'
-import { flexCenter } from '../../constants'
-import { CgProfile } from 'react-icons/cg'
+import { flexCenter,flexBetween } from '../../constants'
 
 
 export const SignUpPageWrapper = styled.div`
@@ -23,22 +22,24 @@ export const SignUpPageLeft = styled.div`
 `
 export const MainTitle = styled.div`
   margin-top : ${props => props.marginTop || '0px'}; 
+  margin-bottom : ${props => props.marginBottom || '0px'}; 
   font-size: ${props => props.fontSize && props.fontSize};
   color: ${props => props.color && props.color};
   font-weight: ${props => props.fontWeight && props.fontWeight};
 `
 
 export const SignUpPageRight = styled.div`
-  position : fixed; 
+  position : absolute; 
   top : 0;
   right : 0;
   width: 50%;
   height: 100vh;
-  padding : 90px 40px 0px 40px;
+  padding : 7.5% 7.5% 0px 7.5%;
   background: white;
 `
 
 export const SignUpPageInnerForm = styled.form`
+  position : relative;
   margin-top: 50px;
 `
 
@@ -46,13 +47,11 @@ export const ProfileWrapper = styled.div`
     ${flexCenter}
     margin : auto; 
     color : grey;
-    ${CgProfile}{
-      cursor : pointer;
-    }
 `; 
 
 export const NickNameBox = styled.input`
-  border : 1px solid #707070;
+  border : none;
+  box-shadow: 4px 4px 10px rgba(0, 0, 0, 0.2);
   width : 404px;
   height : 70px; 
   border-radius : 30px; 
@@ -60,8 +59,9 @@ export const NickNameBox = styled.input`
   font-size : 30px; 
 `;
 
-export const DoubleCheckBox = styled.button`
+export const CheckBox = styled.button`
   height : 70px; 
+  box-shadow: 4px 4px 10px rgba(0, 0, 0, 0.2);
   border-radius : 30px; 
   font-size : 30px; 
   background-color : #CDF6E8;
@@ -70,3 +70,35 @@ export const DoubleCheckBox = styled.button`
   margin-left : 50px;
   font-size : 20px;
 `;
+
+export const JobWrapper = styled.div`
+  ${flexBetween}
+`;
+
+export const JobCheckBtn = styled.button`
+  width : 30%;
+  box-shadow: 4px 4px 10px rgba(0, 0, 0, 0.2);
+  border-radius: 30px;
+  border : none; 
+  background-color: white;
+  padding : 20px;
+  font-size: 20px;
+`; 
+
+export const Introduce = styled.textarea`
+  width : 100%;
+  border : none; 
+  box-shadow: 4px 4px 10px rgba(0, 0, 0, 0.2);
+  min-height : 500px; 
+  font-size : 20px; 
+  border-radius : 30px;
+  padding : 20px; 
+  margin-bottom : 130px
+`;
+
+export const SubmitBtn = styled(CheckBox)`
+  position : absolute; 
+  bottom : 0; 
+  right : 0; 
+  padding : 0px 50px 0px 50px; 
+`

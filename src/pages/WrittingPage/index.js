d2d66081fd85b122110de36776b29b2ff733ced4
colo1211 <colo1211@naver.com>
2021-11-17 21:23:01 +0900

    feat: 글생성페이지 구현중(#10)

--- src/pages/WrittingPage/index.js
+++ src/pages/WrittingPage/index.js
@@ -1,14 +1,86 @@
-import React from 'react';
-import * as S from './style';
+import React, { useState, useCallback } from 'react'
+import * as S from './style'
+import { FaBook } from 'react-icons/fa'
+import { MdPersonAdd } from 'react-icons/md'
+import { HiUserGroup } from 'react-icons/hi'
+import DropDown from '../../components/common/DropDown'
+import { LandingDropdownOptions } from '../../constants'
 
 const WrittingPage = () => {
+
+  // const [checkInputs, setCheckInputs] = useState([]); 
+  
+  // const onChekedHandler = useCallback((checked, id)=> {
+  //   if (checked){
+  //     setCheckInputs([]); 
+  //   }
+  // },[]);
+
   return (
-     <S.WrittingBackGround> 
-      글쓰기 페이지 
+    <S.WrittingBackGround>
       <S.WrittingInnerForm>
-        여기야
+        
+        {/* 모집 유형 */}
+        <S.MainTitle fontSize="30px" marginBottom="3%">
+          모집 유형
+        </S.MainTitle>
+        <S.CategoryWrapper>
+          <S.CategoryCard>
+            <FaBook fontSize="30px" />
+            스터디
+          </S.CategoryCard>
+          <S.CategoryCard>
+            <MdPersonAdd fontSize="30px" />
+            스카웃
+          </S.CategoryCard>
+          <S.CategoryCard>
+            <HiUserGroup fontSize="30px" />
+            프로젝트
+          </S.CategoryCard>
+        </S.CategoryWrapper>
+
+        {/* 제목 */}
+        <S.MainTitle fontSize="30px" marginTop="5%" marginBottom="3%">
+          제목
+        </S.MainTitle>
+        <S.InputBox placeholder="제목을 입력하세요." />
+
+
+        {/* 기술 스택 */}
+        <S.MainTitle fontSize="30px" marginTop="5%" marginBottom="3%">
+          기술 스택
+        </S.MainTitle>
+        <DropDown style={{width : '100%'}} options = {LandingDropdownOptions}></DropDown> 
+
+
+        <S.MiddleWrapper>
+          {/* 회의 환경 */}
+          <S.MeetEnviroment>
+            <S.MainTitle fontSize="30px" marginTop="5%" marginBottom="3%">
+                회의 환경
+            </S.MainTitle>
+            <select style={{width : '100%', padding : '20px', borderRadius : '30px', fontSize : '20px'}}>
+              <option>온라인</option>
+              <option>오프라인</option>
+              <option>혼합</option>
+              <option>상관없음</option>
+            </select>
+           
+            {/* <S.EnviromentCheck id='offline' type='radio'/> <label htmlFor='offline'>오프라인</label>
+            <S.EnviromentCheck id='mix' type='radio'/> <label htmlFor='mix'>혼합</label> */}
+
+
+          </S.MeetEnviroment>
+          {/* 모집인원 */}
+          <S.RecruitPeopleWrapper>
+            <S.MainTitle fontSize="30px" marginTop="5%" marginBottom="3%">
+                 모집인원
+              </S.MainTitle>  
+              <S.InputBox width="30%"></S.InputBox> <span>명</span>
+          </S.RecruitPeopleWrapper>
+        </S.MiddleWrapper>
+
       </S.WrittingInnerForm>
-      
     </S.WrittingBackGround>
   )
 }
--- src/pages/WrittingPage/style.js
+++ src/pages/WrittingPage/style.js
@@ -1,11 +1,76 @@
 import styled from 'styled-components';
-import { flexCenter, flexColumn } from '../../constants';
-
+import { flexCenter, flexColumn, flexBetween } from '../../constants';
 
 export const WrittingBackGround = styled.div`
-    background-color : #F9FBFC; 
+    background-color : #F9FBFC;
+    padding : 5% 10% 5% 10%; 
 `;
 
 export const WrittingInnerForm = styled.div`
     background-color : white;
+    border-radius: 30px;
+    box-shadow : 5px 5px 5px rgba(0,0,0,0.16);
+    padding : 5% 10% 5% 10%; 
+`;
+
+export const CategoryWrapper = styled.div`
+  ${flexBetween};
+  margin-top: 50px;
+`;
+
+export const CategoryCard = styled.div`
+  ${flexColumn};
+  align-items: center;
+  justify-content: center;
+  width: 200px;
+  height: 120px;
+  background-color:#fff;
+  border: 1px solid black;
+  border-radius: 10px;
+  box-shadow:5px 5px 10px rgba(0, 0, 0, 0.2);
+  & + & {
+    margin-left: 150px;
+  }
+`;
+
+export const MainTitle = styled.div`
+  margin-top : ${props => props.marginTop || '0px'}; 
+  margin-bottom : ${props => props.marginBottom || '0px'}; 
+  font-size: ${props => props.fontSize && props.fontSize};
+  color: ${props => props.color && props.color};
+  font-weight: ${props => props.fontWeight && props.fontWeight};
+`
+export const InputBox = styled.input`
+  width : ${props=> props.width || '100%'};   
+  height : 70px;
+  border : none;
+  box-shadow: 4px 4px 10px rgba(0, 0, 0, 0.2);
+  border-radius : 30px; 
+  padding : 0px 30px 0px 30px ;
+  font-size : 30px; 
+`;
+
+export const MiddleWrapper = styled.div`
+  display : flex;
+  margin-top: 5%; 
+`;
+
+export const RecruitPeopleWrapper = styled.div`
+ flex : 1; 
+ background-color: blue;
+`;
+
+export const MeetEnviroment = styled.div`
+ flex : 1.2; 
+ background-color: red;
+`;
+
+export const EnviromentCheck = styled.input`
+  width : 30px; 
+  height : 30px; 
+  border : 1px; 
+`;
+
+export const EnviromentLabel = styled.label`
+
 `;

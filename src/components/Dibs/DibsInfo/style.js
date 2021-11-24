4746e88f8abd0251edd911cb02297e74b1309dfc
hanyoonseok <hys6078@naver.com>
2021-11-24 10:23:08 +0900

    style: DibsPage 미디어 쿼리 적용(#18)

--- src/components/Dibs/DibsInfo/style.js
+++ src/components/Dibs/DibsInfo/style.js
@@ -1,9 +1,17 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 import { flexBetween, flexColumn } from '../../../constants';
+import mediaQuery from '../../../hooks/mediaQuery';
 
 export const DibsInfoWrapper = styled.div`
-width:40%;
-`
+  width: 40%;
+  ${mediaQuery({
+    width: '80%',
+    'background-color': '#fff',
+    'border-radius': '20px',
+    'box-shadow':'3px 3px 20px rgba(0, 0, 0, 0.2)',
+    padding: '2rem 2rem',
+  })}
+`;
 export const Title = styled.label`
   font-size: 2rem;
   font-weight: bold;
@@ -12,6 +20,7 @@ export const Content = styled.div`
   ${flexBetween};
   margin-top: 5rem;
   width: 100%;
+  ${mediaQuery({ 'justify-content': 'space-around' })}
 `;
 export const Type = styled.div`
   ${flexColumn}
--- src/pages/DibsPage/style.js
+++ src/pages/DibsPage/style.js
@@ -8,7 +8,7 @@ export const DibsPageWrapper = styled.div`
   padding: 4rem 16rem 20rem 16rem;
   background-color: #f9fbfc;
   ${flexColumn};
-  ${mediaQuery({ padding: '2rem 4rem 24rem 4rem' })}
+  ${mediaQuery({ padding: '2rem 3rem 24rem 3rem' })}
 `;
 
 export const ContentsWrapper = styled(Paper)`
@@ -17,4 +17,5 @@ export const ContentsWrapper = styled(Paper)`
     ${flexColumn};
     margin-top:5rem;
     padding:4rem 6rem;
+    ${mediaQuery({"box-shadow":"none", "background-color":"inherit", "padding":"0", "align-items":"center"})}
 `

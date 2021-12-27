70393165b5ff5f8a9502e78732defae5a67c475c
hanyoonseok <hys6078@naver.com>
2021-12-27 15:33:42 +0900

    feat: 페이지에 axios 요청 추가(#37)

--- src/pages/MyPage/index.js
+++ src/pages/MyPage/index.js
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import * as S from './style';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
 
-import { BackButton, UserProfile, UserProjects, UserRating,UserDibs } from '../../components';
+import { BackButton, UserProfile, UserProjects, UserRating, UserDibs } from '../../components';
 
 const MyPage = () => {
+  const params = useParams();
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(`/user/${params.id}`)
+      console.log(response.data)
+    };
+    fetchData();
+  }, []);
   const me = {
     name: '김경원',
     avatar: '회원이미지',
     introduce:
       '저는 대학생이고 프론트엔드 개발자가 되기를 희망합니다. 저는 앞으로 계속 나아가는 개발자입니다. 웹에 관심이 많습니다. 많은 관심 부탁드립니다',
-    skills: ['리액트','노드', '리액트','자바스크립트','노드', '리액트', '자바스크립트','노드', '리액트'],
+    skills: [
+      '리액트',
+      '노드',
+      '리액트',
+      '자바스크립트',
+      '노드',
+      '리액트',
+      '자바스크립트',
+      '노드',
+      '리액트',
+    ],
     projects: {
       모집중: [1],
       진행중: [1, 2],
@@ -31,10 +51,10 @@ const MyPage = () => {
       <S.EditProfile>프로필 수정</S.EditProfile>
       <UserProfile me={me} />
       <S.Division>
-        <UserProjects me={me}/>
-        <UserRating me={me}/>
+        <UserProjects me={me} />
+        <UserRating me={me} />
       </S.Division>
-      <UserDibs me={me}/>
+      <UserDibs me={me} />
     </S.MyPageWrapper>
   );
 };

ef9a02b4a06feb91d1628ba842fcf288222e7f5c
hanyoonseok <hys6078@naver.com>
2021-12-30 13:15:34 +0900

    feat: 드롭다운 직접 입력한 것도 배열에 추가(#41)

--- src/components/common/DropDown/index.js
+++ src/components/common/DropDown/index.js
@@ -1,10 +1,9 @@
 import React, { useState, useCallback } from 'react';
 import * as S from './style';
 
-import { SKILL_ID } from '../../../constants';
-
 const DropDown = ({ dropDownOptions, setDropDownOptions, options, ...props }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const [isUserInputOpen, setIsUserInputOpen] = useState(false);
 
   //헤더나 옵션 클릭시 열려진 옵션리스트 닫기용
   const toggle = useCallback(() => {
@@ -13,23 +12,19 @@ const DropDown = ({ dropDownOptions, setDropDownOptions, options, ...props }) =>
 
   //옵션 선택시 헤더 value 변경 + 토글용
   const onOptionClick = useCallback(
-    skillname => () => {
-      let already = false;
+    option => () => {
       if (dropDownOptions.length < 5) {
-        dropDownOptions.forEach(option => {
-          option === skillname ? (already = true) : (already = false);
-        });
-        if (!already) {
-          setDropDownOptions(dropDownOptions.concat(skillname));
+        if (!dropDownOptions.find(e => e === option)) {
+          setDropDownOptions(dropDownOptions.concat(option));
         }
       }
       setIsOpen(false);
     },
-    [dropDownOptions],
+    [dropDownOptions, setDropDownOptions],
   );
-
+  
   const onDeleteClick = useCallback(
-    (e,option) => {
+    (e, option) => {
       e.stopPropagation();
       setDropDownOptions(
         dropDownOptions.filter(
@@ -39,23 +34,39 @@ const DropDown = ({ dropDownOptions, setDropDownOptions, options, ...props }) =>
         ),
       );
     },
-    [dropDownOptions],
+    [dropDownOptions, setDropDownOptions],
   );
 
-  const onUserInputClick = useCallback(() => {});
+  const openUserInput = useCallback(() => {
+    setIsUserInputOpen(true);
+  }, []);
+
+  const onUserInputKeypress = useCallback(
+    e => {
+      const option = e.target.value;
+      if (dropDownOptions.length < 5) {
+        if (!dropDownOptions.find(e => e === option)) {
+          setDropDownOptions(dropDownOptions.concat(option));
+        }
+      }
+      setIsOpen(false);
+      setIsUserInputOpen(false);
+    },
+    [dropDownOptions, setDropDownOptions],
+  );
 
   return (
     <S.DropDownContainer isOpen={isOpen} {...props}>
       <S.DropDownHeader onClick={toggle}>
         {dropDownOptions.map((v, i) => {
           return (
-            <S.HeaderTag onClick={e => onDeleteClick(e,v)} key={i}>
+            <S.HeaderTag onClick={e => onDeleteClick(e, v)} key={i}>
               {v}
             </S.HeaderTag>
           );
         })}{' '}
         &nbsp;
-        <S.HeaderArrow/>
+        <S.HeaderArrow />
       </S.DropDownHeader>
       {isOpen && (
         <S.DropDownList>
@@ -64,7 +75,11 @@ const DropDown = ({ dropDownOptions, setDropDownOptions, options, ...props }) =>
               {option}
             </S.ListItem>
           ))}
-          <S.ListItem onClick={onUserInputClick}>직접입력</S.ListItem>
+          {isUserInputOpen ? (
+            <S.UserInput onKeyPress={e => e.key === 'Enter' && onUserInputKeypress(e)} />
+          ) : (
+            <S.ListItem onClick={openUserInput}>직접입력</S.ListItem>
+          )}
         </S.DropDownList>
       )}
     </S.DropDownContainer>

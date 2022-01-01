99d07a2a64a0bd8b481212f580bb0b8cebfa34ab
hanyoonseok <hys6078@naver.com>
2022-01-01 12:37:49 +0900

    feat: redux-persist 적용(#44)

--- src/index.js
+++ src/index.js
@@ -6,17 +6,22 @@ import createSagaMiddleware from 'redux-saga';
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { Provider } from 'react-redux';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import rootReducer, { rootSaga } from './modules';
 require('dotenv').config();
 
 const sagaMiddleware = createSagaMiddleware();
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(sagaMiddleware)));
+const persistor = persistStore(store);
 sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
 
   document.getElementById('root'),

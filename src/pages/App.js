978385bd0eee0ab289afc25c84f94029d06c8b20
colo1211 <colo1211@naver.com>
2022-01-07 21:48:15 +0900

    feat: URL 수정(#47)

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
-
 import theme from '../common/theme'
 import GlobalStyles from '../common/GlobalStyle'
 import LandingPage from './LandingPage'

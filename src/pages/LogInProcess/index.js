f937fd1cf89bcd99cca56ef87e426f2594a5f608
colo1211 <colo1211@naver.com>
2022-01-20 12:53:40 +0900

    feat: 로그인 accessToken 유효기간 관리(#63)

--- src/pages/LogInProcess/index.js
+++ src/pages/LogInProcess/index.js
@@ -1,41 +1,47 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useParams, useHistory } from 'react-router-dom';
-import { signinUser } from '../../modules/user';
+import { logoutUser, signinUser } from '../../modules/user';
 
 const LogInProcess = () => {
-    // callBack URL 에서 얻을 수 있는 정보
-    // :id/:nickname/:accessToken/:signupCheck/:avatar1/:avatar2/:avatar3
-  
-    const { id, nickname, accessToken, signupCheck, avatar1, avatar2, avatar3 } = useParams();
-    const dispatch = useDispatch(); 
-    const history = useHistory();
-    
-    useEffect(()=> {
-        console.log(JSON.parse(signupCheck)); 
-    },[]); 
-    
-    useEffect(() => {
-        localStorage.setItem('accessToken', accessToken);
-        if (JSON.parse(signupCheck)){
-            const data = {
-                id : parseInt(id), 
-                nickname : nickname,  
-                avatar : avatar1 === '0' ? 'DefaultImg': `/${avatar1}/${avatar2}/${avatar3}`
-            };
-            dispatch(signinUser(data));
-            history.push('/'); 
-        }
-        else {
-            history.push('/signup');
-        }
-    }, []); 
+  // callBack URL 에서 얻을 수 있는 정보
+  // :id/:nickname/:accessToken/:signupCheck/:avatar1/:avatar2/:avatar3
 
-    return (
-        <>
-            <h1>로그인 프로세스</h1>
-        </>
-    )
-}
+  const { id, nickname, accessToken, signupCheck, avatar1, avatar2, avatar3 } =
+    useParams();
+  const dispatch = useDispatch();
+  const history = useHistory();
 
-export default LogInProcess; 
+  useEffect(() => {
+    console.log(JSON.parse(signupCheck));
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('accessToken', accessToken);
+    if (JSON.parse(signupCheck)) {
+      const data = {
+        id: parseInt(id),
+        nickname: nickname,
+        avatar:
+          avatar1 === '0' ? 'DefaultImg' : `/${avatar1}/${avatar2}/${avatar3}`,
+      };
+      dispatch(signinUser(data));
+      history.push('/');
+      setTimeout(() => {
+        localStorage.setItem('accessToken', null);
+        dispatch(logoutUser());
+        window.location.replace(`/login`);
+      }, 3600000);
+    } else {
+      history.push('/signup');
+    }
+  }, []);
+
+  return (
+    <>
+      <h1>로그인 프로세스</h1>
+    </>
+  );
+};
+
+export default LogInProcess;
